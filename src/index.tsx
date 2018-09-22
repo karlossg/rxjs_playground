be115b682b2082d8526dc8db9f47985bcb12908c
Karol <karol.gratus@gmail.com>
2018-09-22 23:14:08 +0200

    6.0+

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { Observable } from "rxjs";
-import config from "recompose/rxjsObservableConfig";
-import { setObservableConfig, componentFromStream } from "recompose";
+import { interval, map } from "rxjs";
+import map from "rxjs/operators/map";
+import { componentFromStream } from "recompose";
 
-setObservableConfig(config);
+const numbers = interval(100);
+// console.log(numbers.subscribe(item => console.log(item)));
 
-const App = componentFromStream(props$ => {
-  return Observable.interval(1000).map(i => <div>{i}</div>);
-});
+const App = componentFromStream(props$ =>
+  numbers.pipe(map((item: number) => <div>{item}</div>))
+);
+
+// const App = (props: any) => (
+//   <div>
+//     <h1>{props.message}</h1>
+//   </div>
+// );
+
+// const createTypeWriter = (message: any, speed: any) =>
+//   Observable.zip(from(message), from(speed), letter => letter).scan(
+//     (acc, curr) => acc + curr
+//   );
+
+// const StreamingApp = componentFromStream(props$ =>
+//   props$.pipe(
+//     switchMap(props => createTypeWriter(props.message, props.speed)),
+//     map(message => ({ message }), map(App))
+//   )
+// );
 
 render(<App />, document.getElementById("root"));
